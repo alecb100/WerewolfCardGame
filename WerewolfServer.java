4cbf50ab39fb109e41a18285dd3559a093e29adf
Alec <acbondi@ncsu.edu>
2023-11-28 19:07:01 -0500

    Implemented base structure of server and client

--- WerewolfServer.java
+++ WerewolfServer.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,8 +11,9 @@ public class WerewolfServer implements Runnable {
     ConcurrentHashMap<String, ObjectOutputStream> players = new ConcurrentHashMap<String, ObjectOutputStream>();
     private ServerSocket ss;
     private ExecutorService clientThreadPool;
-    String gameAction = "";
-    final Object inputLock = new Object();
+    HashMap<String, String> gameActions;
+    boolean gameStart = false;
+    HashMap<String, Boolean> gameWaiting;
 
     public static void main(String[] args) throws IOException {
         new WerewolfServer();
@@ -69,17 +71,18 @@ public class WerewolfServer implements Runnable {
 
 
                 while(true) {
-                    System.out.println("waiting...");
+                    System.out.println("waiting for " + playerName);
                     Object message = input.readObject();
                     String temp = (String)message;
-                    System.out.println(temp);
-                    if(temp.charAt(0) == 'c') {
-                        if(temp.substring(temp.indexOf(">") + 1).equals("")) {
+                    System.out.println(playerName + ": " + temp);
+                    if(gameStart && gameWaiting.get(playerName)) {
+                        if(temp.contains("help")) {
+                            new Thread(new handlePlayerCommand(temp.substring(temp.indexOf(":")+1))).start();
                             continue;
                         }
-                        handlePlayerCommand(name, temp.substring(temp.indexOf(">") + 1));
-                    } else if(temp.charAt(0) == 'a') {
-                        handleGameAction(name, temp.substring(temp.indexOf(">") + 1));
+                        new Thread(new handleGameAction(temp)).start();
+                    } else {
+                        new Thread(new handlePlayerCommand(temp)).start();
                     }
                 }
             } catch(Exception e) {
@@ -88,21 +91,40 @@ public class WerewolfServer implements Runnable {
             }
         }
 
-        private void handleGameAction(String name, String action) {
-            if(!action.equals("")) {
-                synchronized(inputLock) {
-                    gameAction = name + ">" + action;
-                    inputLock.notifyAll();
+        public class handlePlayerCommand implements Runnable {
+            String command;
+
+            public handlePlayerCommand(String command) {
+                this.command = command;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    players.get(playerName).writeObject("We got the message: " + command);
+                    players.get(playerName).flush();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }
 
-        private void handlePlayerCommand(String name, String command) {
-            try {
-                players.get(name).writeObject("m>We got the message" + command);
-                players.get(name).flush();
-            } catch(Exception e) {
-                System.out.println(e.getMessage());
+        public class handleGameAction implements Runnable {
+            String action;
+
+            public handleGameAction(String action) {
+                this.action = action;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    gameActions.replace(playerName, action);
+                    players.get(playerName).writeObject("We got the action: " + action);
+                    players.get(playerName).flush();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
     }
@@ -130,35 +152,34 @@ public class WerewolfServer implements Runnable {
                         System.out.println("Starting");
                         String[] playerArray = new String[server.players.size()];
                         int i = 0;
+                        gameActions = new HashMap<String, String>();
+                        gameWaiting = new HashMap<String, Boolean>();
                         for (String name : server.players.keySet()) {
                             playerArray[i] = name;
+                            gameActions.put(name, "");
+                            gameWaiting.put(name, Boolean.FALSE);
                             i++;
                         }
                         int randomPlayer = (int) (Math.random() * playerArray.length);
                         String randomPlayerName = playerArray[randomPlayer];
-                        server.players.get(randomPlayerName).writeObject("g>Give me 3 (Press enter before giving number)");
+                        server.players.get(randomPlayerName).writeObject("Give me 3 (Press enter before giving number)");
                         server.players.get(randomPlayerName).flush();
+                        gameWaiting.replace(randomPlayerName, Boolean.TRUE);
                         while(true) {
-                            synchronized (inputLock) {
-                                while (gameAction.equals("")) {
-                                    inputLock.wait();
-                                }
-                            }
-                            if(gameAction.contains("3")) {
+                            if(gameActions.get(randomPlayerName).contains("3")) {
                                 break;
                             } else {
-                                server.players.get(randomPlayerName).writeObject("g>Not valid input");
+                                server.players.get(randomPlayerName).writeObject("Not valid input");
                                 server.players.get(randomPlayerName).flush();
                             }
-                            gameAction = "";
+                            gameActions.replace(randomPlayerName, "");
                         }
-                        if (gameAction.substring(0, gameAction.indexOf(">")).equals(randomPlayerName)) {
-                            server.players.get(randomPlayerName).writeObject("><");
+                        if(!gameActions.get(randomPlayerName).equals("")) {
+                            server.players.get(randomPlayerName).writeObject("woohooo!!!");
                             server.players.get(randomPlayerName).flush();
-                            server.players.get(randomPlayerName).writeObject("g>woohooo!!!");
-                            server.players.get(randomPlayerName).flush();
+                            gameWaiting.replace(randomPlayerName, Boolean.FALSE);
                         }
-                        gameAction = "";
+                        gameActions.replace(randomPlayerName, "");
                     }
                 } catch(Exception e) {
                     System.out.println(e.getMessage());
@@ -167,7 +188,3 @@ public class WerewolfServer implements Runnable {
         }
     }
 }
-
-// Redo by taking out the stuff about waiting for a command. Instead, make it so that each player has 1 thread, and the player must do some special
-// input, like help><command> or something. Whenever the server calls for input, it calls a special method which calls the user. If it was a command
-// thing, it gives them the output and them returns a false to the server, which then asks again and calls the method again.
